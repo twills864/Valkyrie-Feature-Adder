9be3886346b45f295cdcc56d904b9eb391c0f529
TJ Wills <twills864@gmail.com>
2021-04-08 17:09:01 -0400

    Adjusted prefab templates to match the prefab field name changes made to the main project.

--- Valkyrie Feature Adder/Program.cs
+++ Valkyrie Feature Adder/Program.cs
@@ -23,8 +23,8 @@ namespace Valkyrie_Feature_Adder
             {
                 LogDirectories();
 
-                CreateFeature();
-                //RunTests();
+                //CreateFeature();
+                RunTests();
             }
             else
             {
@@ -37,7 +37,7 @@ namespace Valkyrie_Feature_Adder
                 Log.WriteLine(PathError, Log.ColorErrorDetails);
             }
 
-            Log.Write("\r\nPress the \"any\" key to continue...", ConsoleColor.White);
+            Log.Write("\r\nPress any key to continue...", ConsoleColor.White);
             Console.ReadKey(true);
         }
 
